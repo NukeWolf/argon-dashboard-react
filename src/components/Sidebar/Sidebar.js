7f5224e842279eda9821318690534a8a5966a8c7
brendxn <brendon.gong@gmail.com>
2022-04-10 00:32:04 -0400

    delete unused buttons and nav

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -239,7 +239,8 @@ const Sidebar = (props) => {
           {/* Divider */}
           <hr className="my-3" />
           {/* Heading */}
-          <h6 className="navbar-heading text-muted">Documentation</h6>
+          
+          {/* <h6 className="navbar-heading text-muted">Documentation</h6> */}
           {/* Navigation */}
           {/*
           <Nav className="mb-md-3" navbar>
